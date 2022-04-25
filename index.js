802b2d81f0331139cfe7f4db0ccb0ad8b43dd06e
hienm9 <98295316+hienm9@users.noreply.github.com>
2022-04-25 00:14:48 -0400

    Add class and constructor for Employee, Engineer, Intern and Manager

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-//Include packages needed for this application
+// Packages needed for this application
 const inquirer = require('inquirer');
 const jest = require('jest');
 const fs = require('fs');
+
+// Constructors
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+
