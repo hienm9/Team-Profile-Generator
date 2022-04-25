8728edf5536967775e96c7053377fe45e8f6c562
hienm9 <98295316+hienm9@users.noreply.github.com>
2022-04-25 11:36:54 -0400

    add engineer team and intern team prompts

--- index.js
+++ index.js
@@ -102,17 +102,117 @@ function init() {
             }
         });
     }
-// TODO add engineer team
+// add engineer team (include name, id, email, github)
     function addEngineer(){
-
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "engineerName",
+                message: "Enter the engineer's name",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter at least one character";
+                }
+            },
+            {
+                type: "input",
+                name: "engineerId",
+                message: "Enter the engineer's id",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter at least one character for id.";
+                }
+            },
+            {
+                type: "input",
+                name: "engineerEmail",
+                message: "Enter the engineer's email",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter at least one character for email.";
+                }
+            },
+            {
+                type: "input",
+                name: "engineerGithub",
+                message: "Enter the engineer's GitHub username",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter at least one character for the GitHub username.";
+                }
+            }
+        ]).then(answers => {
+            const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
+            teamMembers.push(engineer);
+            ids.push(answers.engineerId);
+            createTeam();
+        });
     }
 
-// TODO add intern team
+// add intern team (include name, id, email, school)
     function addIntern() {
-
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "internName",
+                message: "Enter the intern's name",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter at least one character for the name";
+                }
+            },
+            {
+                type: "input",
+                name: "internId",
+                message: "Enter the intern's id",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter at least one character for id.";
+                }
+            },
+            {
+                type: "input",
+                name: "internEmail",
+                message: "Enter the intern's email",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter at least one character for email.";
+                }
+            },
+            {
+                type: "input",
+                name: "internSchool",
+                message: "Enter the intern's school",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter at least one character for the school.";
+                }
+            }
+        ]).then(answers => {
+            const intern = new intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
+            teamMembers.push(intern);
+            ids.push(answers.internId);
+            createTeam();
+        });
     }
 
-// TODO display team
+// TODO display team to a html file to the dist directory
     function displayTeam() {
 
     }
